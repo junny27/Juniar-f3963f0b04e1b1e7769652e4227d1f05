aadbd1d77395ddb2532e0c8e8b8b07442193844f
junny27 <juniar.aliana@gmail.com>
2020-09-03 17:15:42 +0700

    Add files via upload
    front-end

---
+++ front-end/assets/js/actions.js
@@ -0,0 +1,17 @@
+function startTime() {
+  var today = new Date();
+  var date=today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('currentTime').innerHTML =
+  date+" "+h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
